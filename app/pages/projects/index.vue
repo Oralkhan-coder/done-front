b18e378ac702dafce7850e573094c224d8493ef6
oralkh <oralhan.seilhan07@gmail.com>
2026-01-18 13:11:42 +0500

    fix(format)

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <DataTable :value="projectStore.projects" table-style="min-width: 50rem">
-            <Column field="code" header="Code"/>
-            <Column field="title" header="Title"/>
-            <Column field="createdAt" header="Created Date"/>
+            <Column field="code" header="Code" />
+            <Column field="title" header="Title" />
+            <Column field="createdAt" header="Created Date" />
         </DataTable>
     </div>
 </template>
--- app/plugins/api.js
+++ app/plugins/api.js
@@ -1,22 +1,22 @@
 export default defineNuxtPlugin(() => {
-  const authStore = useAuthStore();
+    const authStore = useAuthStore();
 
-  const api = $fetch.create({
-    baseURL: "http://localhost:3000",
-    onRequest({ options }) {
-      if (authStore.accessToken) {
-        options.headers = options.headers || {};
-        options.headers.set("Authorization", `Bearer ${authStore.accessToken}`);
-      }
-    },
-    onResponseError({ response }) {
-      if (response.status === 401) {
-        authStore.logout();
-      }
-    },
-  });
+    const api = $fetch.create({
+        baseURL: 'http://localhost:3000',
+        onRequest({ options }) {
+            if (authStore.accessToken) {
+                options.headers = options.headers || {};
+                options.headers.set('Authorization', `Bearer ${authStore.accessToken}`);
+            }
+        },
+        onResponseError({ response }) {
+            if (response.status === 401) {
+                authStore.logout();
+            }
+        },
+    });
 
-  return {
-    provide: { api },
-  };
+    return {
+        provide: { api },
+    };
 });
